19bc292cc89ede2338cc05244b187267bf1269fa
LazyDoge <lazywoolf@gmail.com>
2020-03-29 15:50:08 +0300

    twosum optimized solution

--- src/main/java/edu/sky/problems/TwoSum.java
+++ src/main/java/edu/sky/problems/TwoSum.java
@@ -1,11 +1,38 @@
 package edu.sky.problems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j && nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+            map.computeIfAbsent(target - nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> integers = map.get(nums[i]);
+            if (integers != null) {
+                if (integers.size() == 1 && integers.get(0) != i) {
+                    return new int[]{i, integers.get(0)};
+                } else if (integers.size() == 2) {
+                    int finalI = i;
+                    Integer secondIndex = integers.stream().filter(integer -> integer != finalI).findFirst().get();
+                    return new int[]{i, secondIndex};
                 }
             }
         }
--- src/test/java/edu/sky/problems/TwoSumTest.java
+++ src/test/java/edu/sky/problems/TwoSumTest.java
@@ -6,12 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumTest {
 
+    private TwoSum twoSum = new TwoSum();
+
+
+
     @Test
     void twoSum() {
-        TwoSum twoSum = new TwoSum();
-        int[] ints = {2, 7, 11, 15};
-        int[] sum = twoSum.twoSum(ints, 9);
-        int[] expected = {0, 1};
+        checkTwoSum(new int[] {2, 7, 11, 15}, 9, new int[]{0,1});
+    }
+
+
+//[0,4,3,0]
+//0
+    @Test
+    void twoSum2() {
+        checkTwoSum(new int[] {0, 4, 3, 0}, 0, new int[]{0,3});
+    }
+
+//    [-3,4,3,90]
+//0
+    @Test
+    void twoSum3() {
+        checkTwoSum(new int[] {-3, 4, 3, 90}, 0, new  int[] {0,2});
+
+    }
+
+
+//    [3,2,4]
+//            6
+    @Test
+    void twoSum4() {
+        checkTwoSum(new int[] {3,2,4}, 6, new int[] {1,2});
+    }
+
+    private void checkTwoSum(int[] array, int target, int[] expected) {
+        int[] sum = twoSum.twoSum(array, target);
         assertArrayEquals(expected, sum);
     }
 }
