e4c34e09168654c0b3f9c5225249f020b2a8aac1
LazyDoge <lazywoolf@gmail.com>
2020-03-29 16:37:47 +0300

    "2. Add Two Numbers" string solution

---
+++ src/main/java/edu/sky/problems/AddTwoNumbers.java
@@ -0,0 +1,65 @@
+package edu.sky.problems;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return stringSolution(l1, l2);
+    }
+
+    private ListNode stringSolution(ListNode l1, ListNode l2) {
+        BigInteger first = getNumber(l1);
+        BigInteger second = getNumber(l2);
+
+        BigInteger sum = first.add(second);
+        String[] split = new StringBuilder(String.valueOf(sum)).reverse().toString().split("");
+        int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+
+        ListNode result = new ListNode(ints[0]);
+        ListNode pointer = result;
+        for (int i = 1; i < ints.length; i++) {
+            pointer.next = new ListNode(ints[i]);
+            pointer = pointer.next;
+        }
+
+        return result;
+
+
+    }
+
+    private BigInteger getNumber(ListNode l1) {
+        StringBuilder sb = new StringBuilder();
+
+        while (l1.next != null) {
+            sb.append(l1.val);
+            l1 = l1.next;
+        }
+        sb.append(l1.val);
+        return new BigInteger(sb.reverse().toString());
+    }
+}
---
+++ src/test/java/edu/sky/problems/AddTwoNumbersTest.java
@@ -0,0 +1,35 @@
+package edu.sky.problems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddTwoNumbersTest {
+
+    private AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+    /*
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 0 -> 8
+     * Explanation: 342 + 465 = 807.
+     */
+    @Test
+    void addTwoNumbers() {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode listNode = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        while (listNode.next != null) {
+            System.out.println("listNode = " + listNode.val);
+            listNode = listNode.next;
+        }
+        System.out.println("listNode.val = " + listNode.val);
+
+    }
+}
